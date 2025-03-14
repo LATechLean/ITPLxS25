d2b9666dd7b602a5df19aa57c601268b239da100
blake-farman <blake.farman@gmail.com>
2025-03-13 19:34:05 -0500

    Removed greater.

--- ITPLxS25/homework/homework00.lean
+++ ITPLxS25/homework/homework00.lean
@@ -1,27 +1,25 @@
 import Mathlib
 
+namespace Homework00
+
 /-
   Suppose we ONLY know how to add two natural numbers.
   Define a function
     double : ℕ → ℕ
   that doubles the input.
 -/
-def double (x : ℕ) : ℕ := sorry
 
+def double (x : ℕ) : ℕ :=
+  sorry
 
 /-
   Define a function
     add : ℕ → ℕ
   that adds two numbers.
 -/
-def add (x y : ℕ) : ℕ := sorry
 
-/-
-  Write a function
-    greater : ℕ → ℕ → ℕ
-  that returns the larger of the two inputs.
--/
-def greater (x y : ℕ) : ℕ := sorry
+def add (x y : ℕ) : ℕ :=
+  sorry
 
 /-
   Suppose that α : Type.
@@ -29,7 +27,8 @@ def greater (x y : ℕ) : ℕ := sorry
     doTwice : (α → α) → α → α
   that applies a function f : α → α to a : α two times.
 -/
-def doTwice (α : Type) (f : α → α) (a : α) : α := sorry
+def doTwice (α : Type) (f : α → α) (a : α) : α :=
+  sorry
 
 /-
   Suppose that α : Type.
@@ -37,14 +36,16 @@ def doTwice (α : Type) (f : α → α) (a : α) : α := sorry
     doThrice : (α → α) → α → α
   that applies a function f : α → α to a : α three times.
 -/
-def doThrice (α : Type) (f : α → α) (a : α) : α := sorry
+def doThrice (α : Type) (f : α → α) (a : α) : α :=
+  sorry
 
 /-
   Write a function
     compose (α β γ : Type) → (β → γ) → (α → β) → α → γ
   that applies the composition of f : α → β an g : β → γ to x : α.
 -/
-def compose (α β γ : Type) (g : β → γ) (f : α → β) (x : α) : γ := sorry
+def compose (α β γ : Type) (g : β → γ) (f : α → β) (x : α) : γ :=
+  sorry
 
 /-
   Suppose we know how to multiply two natural numbers.
@@ -52,4 +53,7 @@ def compose (α β γ : Type) (g : β → γ) (f : α → β) (x : α) : γ := s
     square : ℕ → ℕ
   that squares the input.
 -/
-def square (x : ℕ) : ℕ := sorry
+def square (x : ℕ) : ℕ :=
+  sorry
+
+end Homework00
