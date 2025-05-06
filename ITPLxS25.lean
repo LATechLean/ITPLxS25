79ad917c9089fa86b725155afb217384ad9a60a2
blake-farman <blake.farman@gmail.com>
2025-05-06 06:51:31 -0500

    Updated so lake build will actually build the lecture notes.

--- ITPLxS25.lean
+++ ITPLxS25.lean
@@ -1,3 +1,21 @@
 -- This module serves as the root of the `ITPLxS25` library.
 -- Import modules here that should be built as part of the library.
-import «ITPLxS25».Basic
+-- import «ITPLxS25».Basic
+import ITPLxS25.lectures.Lecture01
+import ITPLxS25.lectures.Lecture02
+import ITPLxS25.lectures.Lecture03
+import ITPLxS25.lectures.Lecture04
+import ITPLxS25.lectures.Lecture05
+import ITPLxS25.lectures.Lecture06
+import ITPLxS25.lectures.Lecture07
+import ITPLxS25.lectures.Lecture08
+import ITPLxS25.lectures.Lecture09
+import ITPLxS25.lectures.Lecture10
+import ITPLxS25.lectures.Lecture11
+import ITPLxS25.lectures.Lecture12
+import ITPLxS25.lectures.Lecture13
+import ITPLxS25.lectures.Lecture14
+import ITPLxS25.lectures.Lecture15
+import ITPLxS25.lectures.Lecture17
+import ITPLxS25.lectures.Lecture18
+import ITPLxS25.lectures.Lecture19
